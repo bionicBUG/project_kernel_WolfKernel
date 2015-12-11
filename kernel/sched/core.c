faf7eec59abef011f15579d2c1ca85d6a2e69770
Juri Lelli <juri.lelli@arm.com>
2015-12-11 11:58:05 +0000

    FIXUP: sched/fair: jump to max OPP when crossing UP threshold
    BUG=chrome-os-partner:44828
    TEST=Boot kernel on Oak.
    
    Change-Id: I1e049d8fce980e265852c74977032f63704bfdf9
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2849,6 +2849,7 @@ void scheduler_tick(void)
 	curr->sched_class->task_tick(rq, curr, 0);
 	update_cpu_load_active(rq);
 	calc_global_load_tick(rq);
+	sched_freq_tick(cpu);
 	raw_spin_unlock(&rq->lock);
 
 	perf_event_task_tick();
@@ -2858,8 +2859,6 @@ void scheduler_tick(void)
 	trigger_load_balance(rq);
 #endif
 	rq_last_tick_reset(rq);
-
-	sched_freq_tick(cpu);
 }
 
 #ifdef CONFIG_NO_HZ_FULL
